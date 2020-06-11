55ebfcbeb815635da78f93d49c1ba02c30925f1b
f-maia <filipe_msr@hotmail.com>
2020-06-11 03:04:51 -0300

    feat(server): Add ORM and database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,20 @@
 import 'dotenv/config';
+import 'reflect-metadata';
+
 import express from 'express';
 import cors from 'cors';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.json({ status: `Server running on ${process.env.APP_URL}` });
+app.get('/test-connection', (req, res) => {
+  return res.json({
+    status: `Server up and running on ${process.env.APP_URL}`,
+  });
 });
 
 app.listen(process.env.APP_PORT);
