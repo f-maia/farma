a8e46b557cdd7011c882960f95bc9a328e5b76b9
f-maia <filipe_msr@hotmail.com>
2020-06-28 19:14:40 -0300

    feat(web): Add routing and custom routing component

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+import Routes from './routes';
 
 function App() {
   return (
-    <div>
-      <h1>Heyy</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
