30545a39c20ce63a27b64f85da25d515058a1f73
f-maia <filipe_msr@hotmail.com>
2020-07-01 03:50:01 -0300

    feat(web): Add style constants and containers

---
+++ web/src/styles/Wrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  height: calc(100% - 7rem);
+  margin-bottom: 7rem;
+
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+export default Wrapper;
---
+++ web/src/styles/constants.js
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    dark_blue: '#1B82A8',
+    light_blue: '#A8E6CF',
+    magenta_pink: '#FE346E',
+    dark_grey: '#474646',
+    middle_grey: '#8F8F8F',
+    light_grey: '#F8F8F8',
+  },
+  paddings: {
+    paddin_little: '1rem',
+    paddin_middle: '1.5rem',
+    paddin_large: '2rem',
+    paddin_X_large: '3rem',
+  },
+  margins: {
+    margin_little: '1rem',
+    margin_middle: '1.5rem',
+    margin_large: '2rem',
+    margin_X_large: '3rem',
+  },
+  animations: {
+    icon_transition: 'all 0.3s linear',
+  },
+};
