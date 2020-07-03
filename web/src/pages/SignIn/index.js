8599fc9dd26bb774fc94eca92e909eeb3b9a6af0
f-maia <filipe_msr@hotmail.com>
2020-07-03 17:33:50 -0300

    feat(web): Integrate SignIn pagee

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,49 +1,76 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
+import * as yup from 'yup';
+
+import { Actions as AuthActions } from '~/store/ducks/auth';
 
 import logo from '~/assets/logo.svg';
 
 import Input from '~/components/Input';
 
 import { Container, OptionsContainer } from './styles';
+import Spinner from '~/components/Spinner';
 
 function SignIn() {
+  const loading = useSelector((state) => state.auth.loading);
+  const dispatch = useDispatch();
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
+
+  function handleSubmit(values) {
+    const { email, password } = values;
+
+    dispatch(AuthActions.signInRequest(email, password));
+  }
+
   return (
     <Container>
       <img src={logo} alt="Busca MED" />
 
       <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={(values, actions) => {
-          console.log(values);
-          console.log(actions);
+        initialValues={{
+          email: '',
+          password: '',
         }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        validateOnChange={false}
+        validateOnBlur={false}
       >
-        <Form>
-          <Field
-            as={Input}
-            type="email"
-            name="email"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-          />
+        {(formikBag) => (
+          <Form>
+            <Field
+              as={Input}
+              type="email"
+              name="email"
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+              error={formikBag.errors.email}
+            />
 
-          <Field
-            as={Input}
-            type="password"
-            name="password"
-            label="Senha"
-            placeholder="Digite sua senha"
-          />
+            <Field
+              as={Input}
+              type="password"
+              name="password"
+              label="Senha"
+              placeholder="Digite sua senha"
+              error={formikBag.errors.password}
+            />
 
-          <button type="submit">Entrar</button>
-        </Form>
+            <button type="submit">{loading ? <Spinner /> : 'Entrar'}</button>
+          </Form>
+        )}
       </Formik>
 
       <OptionsContainer>
         <p>
-          Não tem conta?<Link to="/signup">Crie uma agora</Link>
+          Não tem conta?
+          <Link to="/signup">Crie uma agora</Link>
         </p>
         <Link to="/help">Precisa de ajuda?</Link>
       </OptionsContainer>
